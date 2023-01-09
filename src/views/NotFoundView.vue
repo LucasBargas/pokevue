872ca00bb3feb9c743e9559b47eb94b1ebe8ab9c
LucasBargas <lucas.bargasoficial@gmail.com>
2023-01-09 18:33:27 -0300

    Not found page - developed

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,41 @@
+<script setup>
+import AppContainer from '../components/AppContainer.vue';
+import notFoundIcon from '../assets/images/404.png';
+</script>
+
 <template>
-  <div>
-    <h1>Página 404</h1>
-  </div>
+  <section class="not-found">
+    <AppContainer>
+      <div class="not-found-wrapper">
+        <h1>Sentimos muito, página não encontrada!</h1>
+
+        <figure>
+          <img :src="notFoundIcon" alt="Página não encontrada" />
+        </figure>
+      </div>
+    </AppContainer>
+  </section>
 </template>
+
+<style scoped lang="scss">
+.not-found {
+  padding: 3rem 0;
+
+  &-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    height: 60vh;
+
+    h1 {
+      margin-bottom: 1rem;
+    }
+
+    img {
+      max-width: 180px;
+    }
+  }
+}
+</style>
