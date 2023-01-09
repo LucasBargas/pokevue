edc1e17f6a17ceca27dab43f56ebfda6868436b2
LucasBargas <lucas.bargasoficial@gmail.com>
2023-01-09 19:36:23 -0300

    About page - developed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {reactive} from 'vue'
+import { RouterLink } from 'vue-router';
 import AppContainer from '../components/AppContainer.vue';
-import notFoundIcon from '../assets/images/404.png';
 import AppLoading from '../components/AppLoading.vue';
 import aboutPageIcon from '../assets/images/charizard.png';
 
@@ -23,10 +23,47 @@ setTimeout(() => {
       <div class="about-wrapper">
         <h1>Sobre o PokeVue</h1>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
-          atque, vitae voluptates maxime rerum at ipsum autem id qui eaque
-          eveniet non eos sit pariatur aliquid deserunt.
+          Este App foi desenvolvido por mim, Lucas Bargas, com base em um layout
+          disponibilizado pelo Matheus Battisti, do
+          <a
+            href="https://horadecodar.com.br/hora-de-codar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hora de Codar
+          </a>
+          , onde o mesmo desenvolveu este projeto com Nextjs.
         </p>
+
+        <p>
+          Para estudo decidi fazer este projeto com Vuejs, framework no qual
+          estudo estudando empenhadamente.
+        </p>
+
+        <h2>Para mais informações</h2>
+        <ul>
+          <li>
+            <strong>Repositório do app no github:</strong>
+            <a
+              href="https://github.com/LucasBargas/pokevue"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Cliquei aqui
+            </a>
+          </li>
+          <li>
+            <strong>Meu portfolio completo:</strong>
+            <a
+              href="https://projetoslucasbargas.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Cliquei aqui
+            </a>
+          </li>
+        </ul>
+
         <img :src="aboutPageIcon" alt="About page icon" />
       </div>
     </AppContainer>
@@ -45,14 +82,41 @@ setTimeout(() => {
 
     h1 {
       font-size: 2rem;
+      margin-bottom: 1rem;
     }
 
     p {
-      margin: 1rem 0 1.5rem 0;
       line-height: 1.325rem;
     }
 
+    h2 {
+      margin: 1rem 0 .5rem 0;
+    }
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: .25rem;
+
+      li {
+        display: flex;
+        gap: .25rem;
+
+        a {
+          font-weight: 400;
+        }
+      }
+    }
+
+    a {
+      display: inline-block;
+      color: #333;
+      font-weight: 700;
+    }
+
     img {
+      margin-top: 1.5rem;
       max-width: 300px;
     }
   }
